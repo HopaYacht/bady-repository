7cdbbd378d32ca60dcd73dd64e6b42285178c276
netanel <netanel@hopayacht.com>
2017-03-30 12:18:28 +0300

    this is a testttttttttt

--- src/natan-plugin.js
+++ src/natan-plugin.js
@@ -1,1 +1,42 @@
-export class NatanPlugin {}
+export class NatanPlugin {
+
+    constructor() {
+        this.firstName =  "Netanel" ;
+        this.lastName = "Murciano" ;
+        this.people = [];
+        this.editing = undefined;
+    }
+
+    get fullName(){
+        //return `$(this.firstName) $(this.lastName)` ;
+
+        return this.firstName  + " " +  this.lastName ;
+    }
+
+
+    create() {
+        this.people.push(this.fullName) ;
+
+    }
+
+
+    delete(index) {
+        this.people.splice(index, 1) ;
+
+    }
+
+    edit(index) {
+        this.editing = index ;
+        this.newValue = this.people[index];
+
+    }
+
+    save(index) {
+        this.people[index] = this.newValue;
+        this.editing = undefined;
+
+    }
+
+
+
+}
