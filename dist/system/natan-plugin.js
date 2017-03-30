cb4e54861c9e5a86bfe9d23325a6d07f0109ed60
netanel <netanel@hopayacht.com>
2017-03-30 12:18:55 +0300

    this is a testttttttttt

--- dist/system/natan-plugin.js
+++ dist/system/natan-plugin.js
@@ -1,24 +1,77 @@
 "use strict";
 
 System.register([], function (_export, _context) {
-  "use strict";
+    "use strict";
 
-  var NatanPlugin;
+    var _createClass, NatanPlugin;
 
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
     }
-  }
 
-  return {
-    setters: [],
-    execute: function () {
-      _export("NatanPlugin", NatanPlugin = function NatanPlugin() {
-        _classCallCheck(this, NatanPlugin);
-      });
+    return {
+        setters: [],
+        execute: function () {
+            _createClass = function () {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
+                }
 
-      _export("NatanPlugin", NatanPlugin);
-    }
-  };
+                return function (Constructor, protoProps, staticProps) {
+                    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+                    if (staticProps) defineProperties(Constructor, staticProps);
+                    return Constructor;
+                };
+            }();
+
+            _export("NatanPlugin", NatanPlugin = function () {
+                function NatanPlugin() {
+                    _classCallCheck(this, NatanPlugin);
+
+                    this.firstName = "Netanel";
+                    this.lastName = "Murciano";
+                    this.people = [];
+                    this.editing = undefined;
+                }
+
+                NatanPlugin.prototype.create = function create() {
+                    this.people.push(this.fullName);
+                };
+
+                NatanPlugin.prototype.delete = function _delete(index) {
+                    this.people.splice(index, 1);
+                };
+
+                NatanPlugin.prototype.edit = function edit(index) {
+                    this.editing = index;
+                    this.newValue = this.people[index];
+                };
+
+                NatanPlugin.prototype.save = function save(index) {
+                    this.people[index] = this.newValue;
+                    this.editing = undefined;
+                };
+
+                _createClass(NatanPlugin, [{
+                    key: "fullName",
+                    get: function get() {
+
+                        return this.firstName + " " + this.lastName;
+                    }
+                }]);
+
+                return NatanPlugin;
+            }());
+
+            _export("NatanPlugin", NatanPlugin);
+        }
+    };
 });
